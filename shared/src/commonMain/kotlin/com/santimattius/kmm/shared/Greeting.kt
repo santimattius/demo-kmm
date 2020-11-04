391755ef8b67b412a9b6971de045f42aa3e5ae32
Santiago Mattiauda <santimattius@gmail.com>
2020-11-04 00:51:53 -0300

    update shared code - first step on kmm

--- shared/src/commonMain/kotlin/com/santimattius/kmm/shared/Greeting.kt
+++ shared/src/commonMain/kotlin/com/santimattius/kmm/shared/Greeting.kt
@@ -3,6 +3,7 @@ package com.santimattius.kmm.shared
 
 class Greeting {
     fun greeting(): String {
-        return "Hello, ${Platform().platform}!"
+//        return "Hello, ${Platform().platform}!"
+        return "Guess what it is! > ${Platform().platform.reversed()}!"
     }
 }
